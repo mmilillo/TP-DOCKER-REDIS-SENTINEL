1dd8679b7a03cf05d5158f00b3f0860ab050d461
Manuel Milillo <manuelmilillo@gmail.com>
2022-02-13 03:32:54 -0300

    fix: redis sentil failover funcionando

--- src/main/java/ar/edu/undav/noescalapp/config/RedisConfig.java
+++ src/main/java/ar/edu/undav/noescalapp/config/RedisConfig.java
@@ -2,6 +2,8 @@ package ar.edu.undav.noescalapp.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisSentinelConfiguration;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
@@ -11,8 +13,13 @@ import org.springframework.data.redis.repository.configuration.EnableRedisReposi
 public class RedisConfig {
 
     @Bean
-    public LettuceConnectionFactory redisConnectionFactory() {
-        return new LettuceConnectionFactory(new RedisStandaloneConfiguration("redisDB-master", 6379));
+    public RedisConnectionFactory redisConnectionFactory() {
+        //return new LettuceConnectionFactory(new RedisStandaloneConfiguration("redisDB-master", 6379));
+        RedisSentinelConfiguration sentinelConfig = new RedisSentinelConfiguration().master("my_redis_master")
+        .sentinel("172.20.0.6",26379)
+        .sentinel("172.20.0.7",26380)
+        .sentinel("172.20.0.8",26381);
+        return new LettuceConnectionFactory(sentinelConfig);
     }
 
 
